95426d60990e08b9267b03e7a7a7fc36e8a734fb
kazuma yabuki <kazuma.yabuki@access-company.com>
2021-12-15 20:35:17 +0900

    過去の着手の表示

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -33,6 +33,16 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Go to move #' + move : 'Go to game start';
+      return (
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner != null) {
       status = 'Winner: ' + winner;
@@ -43,14 +53,14 @@ class Game extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board 
+          <Board
             squares={current.squares}
             onClick={(i) => this.handleClick(i)}
           />
         </div>
         <div className="game-info">
-          <div>{ status }</div>
-          <ol>{/* TODO */}</ol>
+          <div>{status}</div>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
