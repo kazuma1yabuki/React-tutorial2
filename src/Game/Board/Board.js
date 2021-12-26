51b3452aeea7068b5815f781a46465a0666d8276
kazuma yabuki <kazuma.yabuki@access-company.com>
2021-12-26 13:45:05 +0900

    手番の処理

--- src/Game/Board/Board.js
+++ src/Game/Board/Board.js
@@ -2,12 +2,33 @@ import React from 'react';
 import Square from './Square/Square';
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      isXturn: true,
+    };
+  }
+
+  handleClick(i) {
+    const newSquares = this.state.squares.slice();
+    newSquares[i] = this.state.isXturn ? 'X' : "O";
+    this.setState({
+      squares: newSquares,
+      isXturn: !this.state.isXturn
+    })
+  }
+
   renderSquare(i) {
-    return <Square value={i}/>;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />);
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player:' + (this.state.isXturn ? "X" : "O");
 
     return (
       <div>
